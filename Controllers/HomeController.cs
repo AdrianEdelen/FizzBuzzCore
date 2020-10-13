db816d2d539e0d5b2067fd86a8eca8b6914c7294
Adrian Edelen <adrianedelen@gmail.com>
2020-10-13 12:50:19 -0400

    Added comments

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,11 +32,14 @@ namespace FizzBuzzCore.Controllers
         [HttpPost]
         public IActionResult Solve(string input1, string input2, string input3, string input4)
         {
+            //catching the vars from the user inputs and converting them to strings
             var min = Convert.ToInt32(input1);
             var max = Convert.ToInt32(input2);
             var fizz = Convert.ToInt32(input3);
             var buzz = Convert.ToInt32(input4);
+            //this will be our output string, using string builder to avoid memory issues if the user inputs very large numbers
             var output = new StringBuilder();
+            //the logic for determining whether fizz or buzz should be printed
             for (int i = min; i <= max; i++)
             {
                 if (i % fizz == 0 && i % buzz == 0)
@@ -56,6 +59,7 @@ namespace FizzBuzzCore.Controllers
                     output.AppendLine(i.ToString());
                 }
             }
+            //outputting to the view
             ViewData["Output"] = output.ToString();
             return View();
         }
